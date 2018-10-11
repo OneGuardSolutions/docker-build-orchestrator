f5037b0c9104cf008a60a04edc916a22646f6a9a
Tomáš Pecsérke <tomas.pecserke@oneguard.email>
2018-10-11 17:01:56 +0200

    publishing to gh-pages

--- build/build.sh
+++ build/build.sh
@@ -3,4 +3,14 @@
 composer install --no-dev --no-interaction \
     || { echo "ERROR: failed to install dependencies" >&2; exit 1; }
 box compile
-sha1sum dobr.phar >> dobr.version
+
+rm gh-pages/ -rf
+git clone https://github.com/OneGuardSolutions/docker-build-orchestrator.git gh-pages -b gh-pages
+
+SUM="$(sha1sum dobr.phar | cut -d ' ' -f 1)"
+VERSION="$(./dobr.phar --version | cut -d ' ' -f 2)"
+URL="https://oneguardsolutions.github.io/docker-build-orchestrator/releases/$VERSION/dobr.phar"
+php "$(dirname "$0")/add-to-manifest.php" dobr.phar "$URL" "$SUM" "$VERSION" gh-pages/manifest.json
+mkdir -p "gh-pages/releases/$VERSION/"
+cp dobr.phar "gh-pages/releases/$VERSION/dobr.phar"
+cp dobr.phar.pubkey "gh-pages/releases/$VERSION/dobr.phar.pubkey"
